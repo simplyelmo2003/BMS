b9674aa02beb981031ef4e31feacd342039ffe73
Lorna <auljee18@gmail.com>
2026-05-01 14:17:13 +0800

    final fix vercel 404

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from '@tanstack/react-router';
-import { getRouter } from './router';
-import './styles.css';
-
-const router = getRouter();
-
-createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
